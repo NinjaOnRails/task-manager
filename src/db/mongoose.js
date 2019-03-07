eb9e0dc136cfa26cc76369acd942d1ca7f1cd64e
Danny Vu <danny.hvu@gmail.com>
2019-03-07 14:27:19 +0100

    Add basic data validation and sanitisation to task and user models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,39 +8,72 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const Task = mongoose.model('Task', {
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 });
 
-// const myTask = new Task({
-//   description: 'Buy eggs',
-//   completed: false
-// });
+const myTask = new Task({
+  description: 'Buy eggs'
+});
 
-// myTask
-//   .save()
-//   .then(() => {
-//     console.log(myTask);
-//   })
-//   .catch(error => {
-//     console.log('Error', error);
-//   });
+myTask
+  .save()
+  .then(() => {
+    console.log(myTask);
+  })
+  .catch(error => {
+    console.log('Error', error);
+  });
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (validator.contains(value, 'password')) {
+        throw new Error('Password must not contain "password"');
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    }
   }
 });
 
 // const me = new User({
 //   name: 'Rick',
-//   age: 'jewaoifjs'
+//   email: 'rick.sanchez@gmail.COM',
+//   password: '    125pas      '
 // });
 
 // me.save()
