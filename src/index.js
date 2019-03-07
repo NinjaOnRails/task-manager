1ccce966839854d7cc00a8b9bb29487cc87dab21
Danny Vu <danny.hvu@gmail.com>
2019-03-07 18:52:37 +0100

    Create users and tasks endpoints

--- src/index.js
+++ src/index.js
@@ -21,6 +21,50 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+  User.find({})
+    .then(users => {
+      res.send(users);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+// app.get('/users/:id', (req, res) => {
+//   const _id = req.params.id;
+
+//   User.findById(_id)
+//     .then(user => {
+//       console.log(user);
+//       if (!user) {
+//         return res.status(404).send();
+//       }
+
+//       res.send(user);
+//     })
+//     .catch(e => {
+//       res.status(500).send();
+//     });
+// });
+
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then(user => {
+      console.log(user);
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send(e);
+    });
+});
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
 
@@ -30,7 +74,34 @@ app.post('/tasks', (req, res) => {
       res.status(201).send(task);
     })
     .catch(e => {
-      res.status(400).send(e);
+      res.status(400).send();
+    });
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then(tasks => {
+      res.send(tasks);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then(task => {
+      console.log(task);
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch(e => {
+      res.status(500).send();
     });
 });
 
