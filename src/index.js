617d7a89f95c2715ed35f08c8133b9e00516db26
Danny Vu <danny.hvu@gmail.com>
2019-03-08 18:07:07 +0100

    Use bcrypt to hash passwords

--- src/index.js
+++ src/index.js
@@ -9,10 +9,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+  const password = 'mypasswordbro';
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  console.log(password);
+  console.log(hashedPassword);
+
+  const isMatch = await bcrypt.compare('mypasswordbro', hashedPassword);
+  console.log(isMatch);
+};
+
+myFunction();
